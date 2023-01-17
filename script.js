72aba28f23bd96c76248701b0c81b638a33be3c1
apersonius <personius12@gmail.com>
2023-01-17 00:34:21 -0600

    write JS and HTML for current date

--- script.js
+++ script.js
@@ -20,4 +20,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  let dt = new Date();
+  document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
 });
